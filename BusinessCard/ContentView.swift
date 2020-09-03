aabe11fc918d1fd7582bbd362c0264c40f1c315d
Mya Bolds <myabolds21@gmail.com>
2020-09-02 19:35:26 -0500

    Initial Commit

---
+++ BusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BusinessCard
+//
+//  Created by Mya B on 9/2/20.
+//  Copyright © 2020 Mya Bolds. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
