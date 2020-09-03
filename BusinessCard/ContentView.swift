e01bc84d449f39f0032bb867d5b535e4c8224c08
Mya Bolds <myabolds21@gmail.com>
2020-09-02 20:01:33 -0500

    Change background color to blue

--- BusinessCard/ContentView.swift
+++ BusinessCard/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(.init(red: 0.45, green: 0.73, blue: 1.00, alpha: 1.00))
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+             Text("Hello, World!")
+        }
+       
     }
 }
 
