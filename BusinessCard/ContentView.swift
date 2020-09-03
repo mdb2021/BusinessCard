8d9615610c76088ddd50588592e167e5b21812bf
Mya Bolds <myabolds21@gmail.com>
2020-09-02 20:29:23 -0500

    Add Google Font in Apply Google Font to my name.

--- BusinessCard/ContentView.swift
+++ BusinessCard/ContentView.swift
@@ -13,7 +13,10 @@ struct ContentView: View {
         ZStack {
             Color(.init(red: 0.45, green: 0.73, blue: 1.00, alpha: 1.00))
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-             Text("Hello, World!")
+             Text("Mya Bolds")
+                .font(Font.custom("Satisfy-Regular", size: 40))
+                .bold()
+                .foregroundColor(.white)
         }
        
     }
